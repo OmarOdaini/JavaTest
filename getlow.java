ad76048880fdbb2c61d141e26a30d30b9b1e0ea1
Omar Odaini <ona11@msstate.edu>
2019-04-07 22:20:27 -0600

    Update getlow.java

--- getlow.java
+++ getlow.java
@@ -31,7 +31,7 @@ public class getlow {
 public static void main(String args[]) {
 	
 	//Points giving
-	int[][] arr = { {5,5}, {-6, -3}, {1, 3}, {4, -5}, {0, 2}};
+	int[][] arr = { {5,5}, {-6, -3}, {8, 3}, {4, 9}, {0, 2}};
 
 	//Declare Table
 	Hashtable<Double, Point> Table = new Hashtable<Double, Point>(); 
